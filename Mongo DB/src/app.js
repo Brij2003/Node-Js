9265a1b4f44cd743ea0f37bc2dd1ce9ae3767e1c
Brij2003 <20bcs033@iiitdwd.ac.in>
2022-01-12 23:34:48 +0530

    mongoose new practice

---
+++ Mongo DB/src/app.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const { getMaxListeners } = require('process');
+const { stringify } = require('querystring');
+
+// creating a new connection with any Database
+mongoose.connect("mongodb://localhost:27017/Demo1").then(async () => await console.log("connection succesfully established.....")).catch(async (err) => await console.log(err));
+
+// defining schema of our Database
+const teamInfo = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    profesion: String,
+    age: Number,
+    mail_id: String,
+    course: String
+});
+
+// Creating a new model of our teamInfo schema 
+const Member = new mongoose.model("Member", teamInfo);
+
+// Creating/inserting new documents
+const createnewDocument = async () => {
+    try {
+        const frontendTeam = new Member({
+            name: "Vijay",
+            profesion: "Student",
+            age: 20,
+            mail_id: "pqr@gmail.com",
+            course: "DSAI"
+        })
+        const backendTeam = new Member({
+            name: "Harsh",
+            profesion: "Student",
+            age: 19,
+            mail_id: "har@gmail.com",
+            course: "CSE"
+        })
+        const result = await Member.insertMany([frontendTeam,backendTeam]);
+        console.log(result);
+    } catch (err)
+    { console.log(err) }
+}
+
+// createnewDocument();
+
+// Reading documents in Database
+
+const getDocument = async () => {
+    try {
+        const result = await Member
+            .find({ course: "CSE" })
+            .select({ name: 1 })
+            .limit(1);
+        console.log(result);
+    } catch (err) {
+        console.log(err)
+    }
+}
+// getDocument();
+
+// Comparision
